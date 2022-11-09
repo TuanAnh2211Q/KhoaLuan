50aedbe6683db6adc0ad81fb376309b5a3e7b678
TuanAnh <tuananh221169@gmail.com>
2022-11-10 04:21:14 +0700

    .

---
+++ Code/QLCHTAN/QLCHTAN/ThongTinKhuyenMai_GUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCHTAN
+{
+    public partial class ThongTinKhuyenMai_GUI : Form
+    {
+        public ThongTinKhuyenMai_GUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
