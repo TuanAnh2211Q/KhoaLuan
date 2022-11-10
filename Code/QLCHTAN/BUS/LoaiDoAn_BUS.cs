4a6525bdafc702a646e6e4ef03a79caf6da5615a
TuanAnh <tuananh221169@gmail.com>
2022-11-10 15:40:12 +0700

    .

---
+++ Code/QLCHTAN/BUS/LoaiDoAn_BUS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+using DAO;
+namespace BUS
+{
+    public class LoaiDoAn_BUS
+    {
+        LoaiDoAn_DAO loaiDoAn_DAO = new LoaiDoAn_DAO();
+
+        public DataTable dsLoaiDoAn_BUS()
+        {
+            return loaiDoAn_DAO.dsLoaiDoAn_DAO();
+        }
+        public bool insert_LoaiDoAn_BUS(LoaiDoAn_DTO loaiDoAn_DTO)
+        {
+            return loaiDoAn_DAO.insert_LoaiDoAn_DAO(loaiDoAn_DTO);
+        }
+    }
+}
--- Code/QLCHTAN/QLCHTAN/Program.cs
+++ Code/QLCHTAN/QLCHTAN/Program.cs
@@ -16,7 +16,7 @@ namespace QLCHTAN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CSDL());
+            Application.Run(new LoaiDoAn_GUI());
         }
     }
 }
