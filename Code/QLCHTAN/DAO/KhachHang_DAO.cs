d0838b5481cfd3574875e588babf5cfa8a148093
VoDien-z <67905632+VoDien-z@users.noreply.github.com>
2022-11-27 23:50:35 +0700

    sa

--- Code/QLCHTAN/DAO/KhachHang_DAO.cs
+++ Code/QLCHTAN/DAO/KhachHang_DAO.cs
@@ -14,23 +14,32 @@ namespace DAO
         public DataTable show_dsKhachHang_DAO()
         {
             Open();
-            SqlDataAdapter da = new SqlDataAdapter("dbo.select_KhachHang", conn);
+            SqlDataAdapter da = new SqlDataAdapter("select_KhachHang", conn);
             DataTable dt = new DataTable();
             da.Fill(dt);
             return dt;
         }
-        public bool update_KhachHang_DAO(KhachHang_DTO khachhang)
+
+        public bool update_KhachHang_DAO(KhachHang_DTO khachhang_DTO)
         {
             Open();
             try
             {
                 SqlCommand cmd = new SqlCommand("update_KhachHang", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
-                //cmd.Parameters.Add("@SDT",sqlDbType.)
+                cmd.Parameters.Add("idKhachHang", SqlDbType.VarChar).Value = khachhang_DTO.IdKhachHang;
+                cmd.Parameters.Add("@SDT", SqlDbType.VarChar).Value = khachhang_DTO.SDT;
+                cmd.Parameters.Add("@tenKhachHang", SqlDbType.VarChar).Value = khachhang_DTO.TenKhachHang;
+                cmd.Parameters.Add("@Phai", SqlDbType.VarChar).Value = khachhang_DTO.Phai;
+                cmd.Parameters.Add("@diaChi", SqlDbType.VarChar).Value = khachhang_DTO.DiaChi;
+                cmd.Parameters.Add("@email", SqlDbType.VarChar).Value = khachhang_DTO.Email;
+                cmd.Parameters.Add("@ghiChu", SqlDbType.VarChar).Value = khachhang_DTO.GhiChu;
+                if (cmd.ExecuteNonQuery() > 0)
+                    return true;
             }
-            catch
+            catch(Exception)
             {
-
+                throw;
             }
             return false;
         }
--- Code/QLCHTAN/DTO/KhachHang_DTO.cs
+++ Code/QLCHTAN/DTO/KhachHang_DTO.cs
@@ -8,21 +8,31 @@ namespace DTO
 {
     public class KhachHang_DTO
     {
+
         private string sDT;
         private string tenKhachHang;
         private string diaChi;
         private string ghiChu;
-
+        private string idKhachHang;
+        private string phai;
+        private string email;
 
         public string TenKhachHang { get => tenKhachHang; set => tenKhachHang = value; }
         public string SDT { get => sDT; set => sDT = value; }
         public string DiaChi { get => diaChi; set => diaChi = value; }
         public string GhiChu { get => ghiChu; set => ghiChu = value; }
-        public KhachHang_DTO(string sDT,string tenKhachHang, string diaChi, string ghiChu)
+        public string IdKhachHang { get => idKhachHang; set => idKhachHang = value; }
+        public string Phai { get => phai; set => phai = value; }
+        public string Email { get => email; set => email = value; }
+
+        public KhachHang_DTO(string idKhachHang,string sDT,string tenKhachHang,string phai, string diaChi,string email, string ghiChu)
         {
+            this.idKhachHang = IdKhachHang;
             this.sDT = SDT;
             this.tenKhachHang = TenKhachHang;
+            this.phai = Phai;
             this.diaChi = DiaChi;
+            this.email = Email;
             this.ghiChu = GhiChu;
         }
     }
